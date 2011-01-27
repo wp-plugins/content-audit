60d4cbc78733972b9c1a100d5210c26cae1f9563
sillybean <sillybean@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-27 20:10:51 +0000

    1.2.1: fixing some small bugs.
    git-svn-id: https://plugins.svn.wordpress.org/content-audit/tags/1.2.1@337948 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ content-audit-report.php
@@ -0,0 +1,254 @@
+<?php
+
+add_action('admin_init', 'content_audit_column_setup');
+
+function content_audit_column_setup() {
+	$options = get_option('content_audit');
+	if ( current_user_can($options['roles']) ) {
+		foreach ($options['types'] as $type => $val) {
+			
+			switch ($type) {
+				case 'post': if (!empty($val)) add_filter('manage_posts_columns', 'content_audit_columns');
+					break;
+				case 'page': if (!empty($val)) add_filter('manage_pages_columns', 'content_audit_columns');
+					break;
+				case 'attachment': if (!empty($val)) add_filter('manage_media_columns', 'content_audit_columns');
+					break;
+				default: add_filter('manage_'.$type.'_posts_columns', 'content_audit_columns');
+			}
+		
+			// fill in the columns
+			// (these three will cover all content types)
+			add_action('manage_posts_custom_column', 'content_audit_custom_column', 10, 2);
+			add_action('manage_pages_custom_column', 'content_audit_custom_column', 10, 2);
+			add_action('manage_media_custom_column', 'content_audit_custom_column', 10, 2);
+		
+			// add filter dropdowns
+			add_action('restrict_manage_posts', 'content_audit_restrict_content_authors');
+			add_action('restrict_manage_posts', 'content_audit_restrict_content_owners');
+			add_action('restrict_manage_posts', 'content_audit_restrict_content_status');
+				
+			// modify edit screens' query when dropdown option is chosen
+			add_filter('posts_where', 'content_audit_posts_where');
+		
+			// Add author field to quick edit
+//			add_action('quick_edit_custom_box', 'add_quickedit_content_owner');
+		}
+	}	
+}
+
+// rearrange the columns on the Edit screens
+function content_audit_columns($defaults) {
+	// preserve the original column headings and remove (unset) default columns
+	$original['comments'] = $defaults['comments'];
+	unset($defaults['comments']);
+	$original['date'] = $defaults['date'];
+	unset($defaults['date']);
+	$original['cb'] = $defaults['cb'];
+	unset($defaults['cb']);
+	if (isset($defaults['categories'])) {
+		$original['categories'] = $defaults['categories'];
+		unset($defaults['categories']);
+	}
+	if (isset($defaults['tags'])) {
+		$original['tags'] = $defaults['tags'];
+		unset($defaults['tags']);
+	}
+	if (isset($defaults['analytics'])) {
+		$original['analytics'] = $defaults['analytics'];
+		unset($defaults['analytics']);
+	}
+	// insert content owner and status taxonomy columns
+	$defaults['content_owner'] = __('Content Owner', 'content-audit');
+    $defaults['content_status'] = __('Content Status', 'content-audit');
+	// restore default columns
+	if (isset($original['categories'])) $defaults['categories'] = $original['categories'];
+	if (isset($original['tags'])) $defaults['tags'] = $original['tags'];
+	$defaults['comments'] = $original['comments'];
+	$defaults['date'] = $original['date'];
+	if (isset($original['analytics'])) $defaults['analytics'] = $original['analytics'];
+	// restore checkbox, add ID as the second column, then add the rest
+    return array('cb' => $original['cb'], 'ID' => __('ID')) + $defaults;
+}
+
+// print the contents of the new Content Audit columns
+function content_audit_custom_column($column_name, $id) {
+	global $post;
+	if ($column_name == 'content_status') {
+		$terms = wp_get_object_terms($post->ID, 'content_audit', array('fields' => 'all'));
+		foreach ($terms as $term) {
+			if (!empty($_GET['post_type'])) $type = 'post_type='.$_GET['post_type'].'&';
+			else $type = '';
+			$termlist[] .= '<a href="edit.php?'.$type.'content_audit='.$term->slug.'">'.$term->name.'</a>';
+		}
+		if (!empty($termlist)) echo implode(', ', $termlist);
+
+		if (isset($_GET['mode']) && ($_GET['mode'] == 'excerpt'))
+			echo '<p>'.get_post_meta($post->ID, "_content_audit_notes", true).'</p>';
+	}
+	elseif ($column_name == 'ID') {
+		echo $post->ID;
+	}
+	elseif ($column_name == 'content_owner') {
+		$ownerID = get_post_meta($post->ID, "_content_audit_owner", true);
+		if (!empty($ownerID) && $ownerID > 0) {
+			if (!empty($_GET['post_type'])) $type = 'post_type='.$_GET['post_type'].'&';
+			else $type = '';
+			echo '<a href="edit.php?'.$type.'content_owner='.$ownerID.'">'.get_the_author_meta('display_name', $ownerID ).'</a>';
+		}
+	}
+}
+
+// print the dropdown box to filter posts by content status
+function content_audit_restrict_content_status() {
+	$options = get_option('content_audit');
+	if (isset($_GET['content_audit'])) $content_status = $_GET['content_audit'];
+	else $content_status = ''; 
+	if (isset($_GET['post_type'])) $type = $_GET['post_type'];
+	else $type = 'post';
+	
+	if ($options['types'][$type] == '1') {
+		?>
+		<select name="content_audit" id="content_audit" class="postform">
+		<option value="0"><?php _e("Show all statuses", 'content-audit'); ?></option>
+	
+		<?php
+		$terms = get_terms( 'content_audit', '' );
+		foreach ($terms as $term) { ?>
+			<option value="<?php echo $term->slug; ?>" <?php selected($term->slug, $content_status) ?>><?php echo $term->name; ?></option>
+<?php	}
+		?>
+		</select>
+	<?php
+	}
+}
+
+// print the dropdown box to filter posts by content owner
+function content_audit_restrict_content_owners() {
+	global $user_ID;
+	$options = get_option('content_audit');
+	if (isset($_GET['content_owner'])) $owner = $_GET['content_owner'];
+	else $owner = '0'; 
+	if (isset($_GET['post_type'])) $type = $_GET['post_type'];
+	else $type = 'post';
+	
+	if ($options['types'][$type] == '1') {
+		wp_dropdown_users(
+			array(
+				'show_option_all' => __('Show all owners', 'content-audit'),
+				'name' => 'content_owner',
+				'selected' => isset($_GET['content_owner']) ? $_GET['content_owner'] : 0
+			)
+		);
+	}
+}
+
+// print a dropdown to filter posts by author
+function content_audit_restrict_content_authors()
+{
+	global $user_ID; 
+	$editable_ids = get_editable_user_ids( $user_ID );
+	wp_dropdown_users(
+		array(
+			'include' => $editable_ids,
+			'show_option_all' => __('Show all authors', 'content-audit'),
+			'name' => 'author',
+			'selected' => isset($_GET['author']) ? $_GET['author'] : 0
+		)
+	);
+}
+
+// amend the db query based on content owner dropdown selection
+function content_audit_posts_where($where)
+{
+	global $wpdb;
+	if (isset($_GET['content_owner']) && !empty($_GET['content_owner'])) { 
+		$where .= " AND ID IN (SELECT post_id FROM {$wpdb->postmeta} WHERE meta_key='_content_audit_owner' AND meta_value='{$_GET['content_owner']}' )";
+	}	
+	return $where;
+}
+
+// Outputs the new custom Quick Edit field HTML
+function add_quickedit_content_owner($column_name, $type) { 
+	if ($column_name == 'content_owner') {
+		global $post, $user_ID;
+		$owner = get_post_meta($post->ID, '_content_audit_owner', true);		
+		?>
+	<fieldset class="inline-edit-col-right">
+	    <div class="inline-edit-col">
+		<label class="inline-edit-status alignleft">
+			<span class="title"><?php _e("Content Owner", 'content-audit'); ?></span>
+			<?php
+			$editable_ids = get_editable_user_ids( $user_ID );
+			wp_dropdown_users(
+				array(
+					'include' => $editable_ids,
+					'show_option_all' => __('None', 'content-audit'),
+					'name' => '_content_audit_owner',
+					'selected' => $owner
+				)
+			);			
+			?>
+			</label>
+		</div>
+	</fieldset>		
+<?php }
+}
+
+// Prints the content status, notes, and owner on the front end
+function content_audit_front_end_display($content) {
+	$options = get_option('content_audit');
+	if (!empty($options['display_switch']) && (current_user_can($options['roles']))) {
+		global $post;
+		$out = '<p class="content-status">'.get_the_term_list($post->ID, 'content_audit', __('Content status: ', 'content-audit'), ', ','').'</p>';
+		$ownerID = get_post_meta($post->ID, "_content_audit_owner", true);
+		if (!empty($ownerID)) {
+			$out .= '<p class="content-owner">'.__("Assigned to: ", 'content-audit').get_the_author_meta('display_name', $ownerID).'</p>';
+		}
+		$out .= '<p class="content-notes">'.get_post_meta($post->ID, "_content_audit_notes", true).'</p>';
+		$out = '<div class="content-audit">'.$out.'</div>';
+		
+		$css = '<style type="text/css">'.$options['css'].'</style>';
+		
+		if ($options['display'] == 'above') return $out.$content;
+		elseif ($options['display'] == 'below') return $content.$out;
+		else return $content;
+	}
+	else return $content;
+}
+
+add_filter('the_content', 'content_audit_front_end_display');
+
+// Prints the CSS for the front end
+function content_audit_front_end_css() {
+	$options = get_option('content_audit');
+	if (!empty($options['display']) && (current_user_can($options['roles']))) {	
+		echo '<style type="text/css">'.$options['css'].'</style>';
+	}
+}
+add_action('wp_head', 'content_audit_front_end_css');
+
+// Dashboard Widget
+function content_audit_dashboard_widget() {
+	$options = get_option('content_audit');
+	foreach ($options['types'] as $type => $val) {
+		if ($val == '1') {
+			$oldposts = get_posts('numberposts=5&post_type='.$type.'&content_audit=outdated&order=ASC&orderby=modified');
+			$obj = get_post_type_object( $type );
+			echo '<table class="widefat fixed" id="content-audit-outdated"><thead><tr><th>'.$obj->label.'</th><th  class="column-date">'.__('Last Modified', 'content-audit').'</th></tr></thead><tbody>';
+			foreach ($oldposts as $apost) {
+				echo '<tr class="author-self"><td class="column-title"><a href="'.get_permalink($apost->ID).'">'.$apost->post_title.'</a></td>';
+				echo '<td class="column-date">'. mysql2date(get_option('date_format'), $apost->post_modified).'</td></tr>';
+			}
+			echo '<tr><td class="column-title" colspan="2"><a href="edit.php?post_type='.$type.'&content_audit=outdated">'.__('See all...', 'content-audit').'</a></td></tr></tbody></table>';
+		}
+	}
+}
+
+function content_audit_dashboard_widget_setup() {
+    wp_add_dashboard_widget( 'dashboard_audit_widget_id', __('Outdated Content', 'content-audit'), 'content_audit_dashboard_widget');
+}
+
+add_action('wp_dashboard_setup', 'content_audit_dashboard_widget_setup');
+
+?>
---
+++ content-audit.php
@@ -0,0 +1,120 @@
+<?php
+/*
+Plugin Name: Content Audit
+Plugin URI: http://sillybean.net/code/wordpress/content-audit/
+Description: Lets you create a content inventory and notify the responsible parties about their outdated content. 
+Version: 1.2.1
+Author: Stephanie Leary
+Author URI: http://sillybean.net/
+
+Copyright 2010  Stephanie Leary  (email : steph@sillybean.net)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/*
+TODO:
+* In quick edit, use JS to select current content owner (if any)
+*/
+
+// when activated, add option
+register_activation_hook(__FILE__, 'content_audit_activation');
+function content_audit_activation() {
+	if (!function_exists('register_taxonomy_for_object_type')) {
+		deactivate_plugins(basename(__FILE__)); // Deactivate myself
+		wp_die("Sorry, but you can't run this plugin, it requires WordPress 3.0.");
+	}
+	// set defaults
+	$options = array();	
+	$options['types'] = array('page' => 1);
+	$options['roles'] = 'edit_pages';
+	$options['display_switch'] = '0';
+	$options['display'] = '0';
+	$options['css'] = 'div.content-audit { background: #ffc; }
+p.content-notes { font-style: italic; }';
+	$options['mark_outdated'] = 0;
+	$options['outdate'] = 1;
+	$options['outdate_unit'] = 'years';
+	$options['notify'] = 0;
+	$options['notify_authors'] = 0;	
+	$options['interval'] = 'monthly';	
+	// set the new option
+	add_option('content_audit', $options, '', 'yes');
+}
+
+// register options
+add_action('admin_init', 'register_content_audit_options' );
+function register_content_audit_options(){
+	register_setting( 'content_audit', 'content_audit' );
+}
+
+// when uninstalled, remove option
+register_uninstall_hook( __FILE__, 'content_audit_delete_options' );
+function content_audit_delete_options() {
+	delete_option('content_audit');
+}
+
+// add settings link to plugin list
+add_filter('plugin_action_links', 'content_audit_plugin_actions', 10, 2);
+function content_audit_plugin_actions($links, $file) {
+ 	if ($file == 'content-audit/content-audit.php' && function_exists("admin_url")) {
+		$settings_link = '<a href="' . admin_url('options-general.php?page=content-audit') . '">' . __('Settings', 'content-audit') . '</a>';
+		array_unshift($links, $settings_link); 
+	}
+	return $links;
+}
+
+// add the pages to the navigation menu
+add_action('admin_menu', 'content_audit_add_pages');
+
+function content_audit_add_pages() {
+    // Add a new submenu under Options:
+	$css = add_options_page(__('Content Audit Options', 'content-audit'), __('Content Audit', 'content-audit'), 'manage_options', 'content-audit', 'content_audit_options');
+	// Add CSS to some specific admin pages
+	add_action("admin_head-$css", 'content_audit_css');
+	add_action("admin_head-post.php", 'content_audit_css');
+	add_action("admin_head-edit.php", 'content_audit_css');
+	add_action("admin_head-index.php", 'content_audit_css');
+}
+
+function content_audit_css() {	?>
+	<style type="text/css">
+	#content_audit_types li { display: inline; padding-right: 2em; }
+	#content_audit_meta label { padding-right: 2em; }
+	#content_audit_meta, #content_audit_meta .inside { overflow: auto; }
+	#audit-notes { width: 100%; }
+	#audit-notes textarea { width: 95%; margin: 0 1em 1em 0; }
+	#content_audit_form textarea { display: block; width: 30em; height: 10em; }
+	#posts-filter th#ID { width: 4em; }
+	table#content-audit-outdated { border: 0; }
+	table#content-audit-outdated td.column-title { padding: 8px .5em; }
+	table#content-audit-outdated td.column-date { padding: 8px .5em 8px 0; width: 30%; }
+	table#content-audit-outdated th.column-date { text-align: left; width: 30%; padding: 0; }
+	</style>
+<?php 
+}
+
+// i18n
+if (!defined('WP_PLUGIN_DIR'))
+	define('WP_PLUGIN_DIR', dirname(dirname(__FILE__))); 
+$lang_dir = basename(dirname(__FILE__)). '/languages';
+load_plugin_textdomain( 'content-audit', 'WP_PLUGIN_DIR'.$lang_dir, $lang_dir );
+
+// load stuff
+include_once(dirname (__FILE__)."/content-audit-fields.php");
+include_once(dirname (__FILE__)."/content-audit-options.php");
+include_once(dirname (__FILE__)."/content-audit-report.php");
+include_once(dirname (__FILE__)."/content-audit-schedule.php");
+?>
